808e7e76bf26f57be931f8ab783e6f9fb5fd62b0
AmbreCorreia <74357454+AmbreCorreia@users.noreply.github.com>
2022-11-21 21:58:00 +0100

    Client OpenStreetMap

--- C#/ApplicationServer/ClientOpenStreetMapAPI.cs
+++ C#/ApplicationServer/ClientOpenStreetMapAPI.cs
@@ -10,7 +10,49 @@ namespace ApplicationServer
     internal class ClientOpenStreetMapAPI
     {
         private static readonly HttpClient client = new HttpClient();
+        private static readonly string API_KEY = "5b3ce3597851110001cf6248533c8f297d74424baa814af18ec650eb";
 
-        
+        public async Task<Location[]> getLocation(string address)
+        {
+            string url = "https://nominatim.openstreetmap.org/?";
+            string text = "&addressdetails=1" + "&q=" + address + "&format=json" + "&limit=1";
+            var baseAddress = new Uri(url + text);
+
+            
+            client.DefaultRequestHeaders.Clear();
+            client.DefaultRequestHeaders.TryAddWithoutValidation("accept", "*/*");
+            client.DefaultRequestHeaders.TryAddWithoutValidation("accept-Encoding", "gzip, deflate, br");
+            client.DefaultRequestHeaders.TryAddWithoutValidation("connection", "keep-alive");
+            client.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent", "PostmanRuntime/7.29.0");
+            var response = await client.GetAsync(baseAddress);
+            string responseData = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<Location[]>(responseData);
+        }
+
+
+        public async Task<Itinerary> getItinary(string start, string end, Boolean bicycle)
+        {
+            Task<Location[]> startingPosition = getLocation(start);
+            Task<Location[]> endPosition = getLocation(end);
+            startingPosition.Wait();
+            endPosition.Wait();
+            string url = "https://api.openrouteservice.org/v2/directions/" + (bicycle ? "cycling-regular" : "foot-walking") + "?api_key=";
+            string text = "&start=" + startingPosition.Result[0].lon + "," + startingPosition.Result[0].lat + "&end=" +
+                endPosition.Result[0].lon + "," + endPosition.Result[0].lat + "&size=1";
+            var baseAddress = new Uri(url + API_KEY + text);
+
+            using (var httpClient = new HttpClient { BaseAddress = baseAddress })
+            {
+                httpClient.DefaultRequestHeaders.Clear();
+                httpClient.DefaultRequestHeaders.TryAddWithoutValidation("accept", "application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8");
+
+                using (var response = await httpClient.GetAsync(""))
+                {
+                    string responseData = await response.Content.ReadAsStringAsync();
+                    return JsonConvert.DeserializeObject<Itinerary>(responseData);
+                }
+            }
+        }
     }
 }
+
---
+++ C#/ApplicationServer/OpenStreetMapClasses.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationServeur
+{
+    [Serializable]
+    public class Geopoints
+    {
+        public Geopoints(){}
+       
+        public Location[] loc { get; set; }
+    }
+
+    [Serializable]
+    public class Location
+    {
+        public Location(){}
+      
+        public int place_id { get; set; }
+        public string licence { get; set; }
+        public string osm_type { get; set; }
+        public int osm_id { get; set; }
+        public string[] boundingbox { get; set; }
+        public string lat { get; set; }
+        public string lon { get; set; }
+        public string display_name { get; set; }
+        public string _class { get; set; }
+        public string type { get; set; }
+        public float importance { get; set; }
+        public string icon { get; set; }
+        public Address address { get; set; }
+    }
+
+    [Serializable]
+    public class Address
+    {
+        public Address(){}
+       
+        public string shop { get; set; }
+        public string road { get; set; }
+        public string neighbourhood { get; set; }
+        public string suburb { get; set; }
+        public string borough { get; set; }
+        public string city { get; set; }
+        public string ISO31662lvl4 { get; set; }
+        public string postcode { get; set; }
+        public string country { get; set; }
+        public string country_code { get; set; }
+    }
+
+    [Serializable]
+    public class Itinerary
+    {
+        public Itinerary(){}
+      
+        public Geocoding geocoding { get; set; }
+        public string type { get; set; }
+        public List<Feature> features { get; set; }
+        public List<double> bbox { get; set; }
+    }
+
+    [Serializable]
+    public class Geocoding
+    {
+        public Geocoding(){}
+  
+        public string version { get; set; }
+        public string attribution { get; set; }
+        public Query query { get; set; }
+        public Engine engine { get; set; }
+        public long timestamp { get; set; }
+    }
+
+    [Serializable]
+    public class Query
+    {
+        public Query() {}
+
+        public int size { get; set; }
+        public bool _private { get; set; }
+        public double pointlat { get; set; }
+        public double pointlon { get; set; }
+        public double boundarycirclelat { get; set; }
+        public double boundarycirclelon { get; set; }
+        public Lang lang { get; set; }
+        public int querySize { get; set; }
+    }
+
+    [Serializable]
+    public class Lang
+    {
+        public Lang(){}
+
+        public string name { get; set; }
+        public string iso6391 { get; set; }
+        public string iso6393 { get; set; }
+        public string via { get; set; }
+        public bool defaulted { get; set; }
+    }
+
+    [Serializable]
+    public class Engine
+    {
+        public Engine(){}
+
+        public string name { get; set; }
+        public string author { get; set; }
+        public string version { get; set; }
+    }
+
+    [Serializable]
+    public class Feature
+    {
+        public Feature(){}
+
+        public string type { get; set; }
+        public Geometry geometry { get; set; }
+        public Properties properties { get; set; }
+        public List<double> bbox { get; set; }
+    }
+
+    [Serializable]
+    public class Geometry
+    {
+        public Geometry(){}
+
+        public string type { get; set; }
+        public List<List<double>> coordinates { get; set; }
+    }
+
+    [Serializable]
+    public class Properties
+    {
+        public Properties(){}
+
+        public string id { get; set; }
+        public string gid { get; set; }
+        public string layer { get; set; }
+        public string source { get; set; }
+        public string source_id { get; set; }
+        public string name { get; set; }
+        public double confidence { get; set; }
+        public double distance { get; set; }
+        public string accuracy { get; set; }
+        public string country { get; set; }
+        public string country_gid { get; set; }
+        public string country_a { get; set; }
+        public string macroregion { get; set; }
+        public string macroregion_gid { get; set; }
+        public string macroregion_a { get; set; }
+        public string region { get; set; }
+        public string region_gid { get; set; }
+        public string region_a { get; set; }
+        public string localadmin { get; set; }
+        public string localadmin_gid { get; set; }
+        public string locality { get; set; }
+        public string locality_gid { get; set; }
+        public string borough { get; set; }
+        public string borough_gid { get; set; }
+        public string neighbourhood { get; set; }
+        public string neighbourhood_gid { get; set; }
+        public string continent { get; set; }
+        public string continent_gid { get; set; }
+        public string label { get; set; }
+    }
+}
