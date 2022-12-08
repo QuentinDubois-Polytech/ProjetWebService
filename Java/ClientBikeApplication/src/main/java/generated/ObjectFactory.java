331435a766400f02517e0933e2f56c6ed736890e
QuentinDubois <quentin.dubois@etu.univ-cotedazur.fr>
2022-12-08 18:23:08 +0100

    changement de la version d'un plugin com.sun.xml.ws

--- Java/ClientBikeApplication/src/main/java/generated/ObjectFactory.java
+++ Java/ClientBikeApplication/src/main/java/generated/ObjectFactory.java
@@ -3,12 +3,12 @@ package generated;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.annotation.XmlElementDecl;
+import javax.xml.bind.annotation.XmlRegistry;
 import javax.xml.datatype.Duration;
 import javax.xml.datatype.XMLGregorianCalendar;
 import javax.xml.namespace.QName;
-import jakarta.xml.bind.JAXBElement;
-import jakarta.xml.bind.annotation.XmlElementDecl;
-import jakarta.xml.bind.annotation.XmlRegistry;
 
 
 /**
--- Java/ClientBikeApplication/src/main/java/generated/package-info.java
+++ Java/ClientBikeApplication/src/main/java/generated/package-info.java
@@ -1,2 +1,2 @@
-@jakarta.xml.bind.annotation.XmlSchema(namespace = "http://schemas.datacontract.org/2004/07/ApplicationServer", elementFormDefault = jakarta.xml.bind.annotation.XmlNsForm.QUALIFIED)
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://schemas.datacontract.org/2004/07/ApplicationServer", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
 package generated;
